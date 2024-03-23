be0cdf60e568d8e40edbaa352059408bd6f04979
hasanalam24 <hasanalam365@gmail.com>
2024-03-24 00:03:00 +0600

    navbar daisy ui, i am not create

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Blogs from './Pages/Blogs'
+import Bookmarks from './Pages/Bookmarks'
+import Home from './Pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+      {/* <h1 className='text-3xl font-bold'>Hello World</h1> */}
+
+      <Home></Home>
+
+      <Blogs></Blogs>
+
+      <Bookmarks></Bookmarks>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Bookmarks.jsx
@@ -0,0 +1,11 @@
+
+
+const Bookmarks = () => {
+    return (
+        <div>
+            <h1>Bookmark page</h1>
+        </div>
+    );
+};
+
+export default Bookmarks;
