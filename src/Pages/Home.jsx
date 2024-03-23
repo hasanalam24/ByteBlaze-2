51f28e94b1d7aec433e895b39f042eeb30b18bd5
hasanalam24 <hasanalam365@gmail.com>
2024-03-24 01:38:27 +0600

    heros bg animated svg set

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import Heros from "../Components/Heros";
-
+import wave from '../assets/wave.svg'
 
 const Home = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[calc(100vh-284px)]">
+        <div className="flex relative flex-col items-center justify-center min-h-[calc(100vh-284px)]">
             <Heros></Heros>
+
+            <img className="absolute bottom-0 w-full" src={wave} alt="" />
         </div>
     );
 };
